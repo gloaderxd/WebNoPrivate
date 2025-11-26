5f581e177328ce4c5beae27096950ef570d5d91f
gloaderxd <ascaniofrancys483@gmail.com>
2025-11-26 16:10:01 -0400

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+      function whatsapp() {
+        location.href = ("https://whatsapp.com/channel/0029Vb6o2K57YSd5yDEp7g1E")
+      }
+      function telegram() {
+        location.href = ("https://t.me/AVESTRUUS")
+      }
